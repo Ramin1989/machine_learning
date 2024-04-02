99e7f5e262f9b9c3440e4fac2644c6a3f95daeda
Mohammad Sarraf <m.sheikhsarraf@gmail.com>
2024-04-02 19:46:44 +0200

    add Decision Tree classifier

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
 
 
@@ -26,7 +28,7 @@ def standardize_data(data: pd.DataFrame) -> np.ndarray:
 if __name__ == "__main__":
     path_to_data = "heart.csv"
     heart_data = load_csv(path_to_data)
-    
+    print(heart_data.head())
     feature_columns = [column_name for column_name in heart_data.columns if column_name != 'output']
 
     stdandardized_columns = standardize_data(heart_data[feature_columns])
@@ -38,15 +40,23 @@ if __name__ == "__main__":
     # print(f'x_train: {np.shape(X_train)}')
     # print(f'x_test: {np.shape(X_test)}')
 
-    classifer = KNeighborsClassifier(n_neighbors=10)
-    classifer.fit(X_train, y_train)
+    kn_classifer = KNeighborsClassifier(n_neighbors=5, weights='distance', p=1)
+    kn_classifer.fit(X_train, y_train)
 
-    predictions = classifer.predict(X_test)
+    kn_predictions = kn_classifer.predict(X_test)
 
-    accuracy = accuracy_score(y_test, predictions)
+    kn_accuracy = accuracy_score(y_test, kn_predictions)
 
-    print(predictions)
-    print(accuracy)
+    print(f'K nearest neighbors classifer accuracy: {kn_accuracy}')
+
+    dt_classifer = DecisionTreeClassifier(random_state=10, max_depth=3)
+    dt_classifer.fit(X_train, y_train)
+
+    dt_perdictions = dt_classifer.predict(X_test)
+
+    dt_accuracy = accuracy_score(y_test, dt_perdictions)
+
+    print(f'Decision Tree classifer accuracy: {dt_accuracy}')
 
 
 
